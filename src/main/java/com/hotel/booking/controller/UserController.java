bac0d3e0eea4bb84944eb7546dc12677115884b6
LakshmipriyaRajasekar <161698123+LakshmipriyaRajasekar@users.noreply.github.com>
2024-03-14 07:01:27 +0530

    booking initial version

---
+++ src/main/java/com/hotel/booking/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.hotel.booking.controller;
+
+import com.hotel.booking.pojo.User;
+import com.hotel.booking.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(produces = "application/json")
+public class UserController {
+    @Autowired
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping(value = "/users")
+    public List getUsers(){
+        List users = userRepository.findAll();
+        return users;
+    }
+
+    @PostMapping(value = "/users")
+    public User newUser(@RequestBody User user){
+        this.userRepository.add(user);
+        return user; // new ResponseEntity(user, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/users/{id}")
+    public ResponseEntity updateUser(@PathVariable int id, @RequestBody User user){
+        User updateduser = this.userRepository.updateUser(id, user);
+        if(updateduser != null){
+            return  new ResponseEntity(user, HttpStatus.ACCEPTED);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping
+    public ResponseEntity deleteUser(@PathVariable int id){
+        boolean result = this.userRepository.deleteUser(id);
+        if(result)
+            return ResponseEntity.noContent().build();
+        else
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+
+}
---
+++ src/main/java/com/hotel/booking/pojo/User.java
@@ -0,0 +1,17 @@
+package com.hotel.booking.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private int id;
+    private String name;
+    private String mailId;
+}
